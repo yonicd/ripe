0e5c5f4a4768f3bda59c5e5547ad328a1802d3b0
Jonathan Sidi <yonis@metrumrg.com>
2018-08-17 19:21:34 -0400

    replace ripley with deparse,subsitute

--- R/ripe.R
+++ R/ripe.R
@@ -3,7 +3,6 @@
 #' @param x chain
 #' @param f replication function
 #' @param \dots arguments to pass to f
-#' @details \code{ripely} is for *apply family of functions.
 #' @return object that f outputs
 #' @examples 
 #' stats::runif(20)%>%
@@ -12,25 +11,28 @@
 #'  
 #' stats::runif(20)%>%
 #'  sample(4)%>%
-#'  ripely(lapply,X=1:4)
+#'  ripe(lapply,X=1:4)
 #' @seealso 
 #'  \code{\link[purrr]{rerun}}
 #' @rdname ripe
 #' @export 
 ripe <- function(x,f,...){
   cp <- chain_parts(x)
-  f_ <- function(){pipe_(cp)}
-
-  f(f_(),...)
-
-}
-
-#' @rdname ripe
-#' @export 
-ripely <- function(x,f,...){
-  cp <- chain_parts(x)
-  f_ <- function(x){pipe_(cp)}
   
-  f(FUN=f_,...)
+  fname <- deparse(substitute(f))
+  
+  if(grepl('apply$',fname)){
+    
+    fapply_ <- function(x){pipe_(cp)}
+    
+    f(FUN=fapply_,...)
+    
+  }else{
+    
+    f_ <- function(){pipe_(cp)}
+    
+    f(f_(),...)  
+    
+  }
   
 }
