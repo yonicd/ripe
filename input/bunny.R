4cc159dfe88094896adfc1fc253a320525422973
Jonathan Sidi <yonis@metrumrg.com>
2019-11-21 09:45:01 -0500

    add logo to readme

--- input/bunny.R
+++ input/bunny.R
@@ -7,20 +7,16 @@ y <- magick::image_read("input/replay.png")
 x%>% magick::image_scale("200x200")
 y%>% magick::image_scale("200x200")
 
-hex_canvas <- image_canvas_hex(border_color="#0d4448", border_size = 2, fill_color = "#ffffff")
+hex_canvas <- image_canvas_hex(border_color="#0d4448", border_size = 2, fill_color = "#ede6f2")
 hex_border <- image_canvas_hexborder(border_size = 1)
 
 img_hex <- hex_canvas %>%
   bunny::image_compose(y%>%magick::image_scale('1300x1300'),
                        gravity = "center", offset = '+0-50')%>%  
-  magick::image_annotate("%>%", size=250, 
-                         weight = 700,
-                         gravity = "center", 
-                         location = '-300+100',
-                         degrees = 350,
-                         font = "Aller")%>%
-  bunny::image_compose(hex_border, gravity = "center", operator = "Over")%>%
-  magick::image_scale("200x200")
+  magick::image_annotate("%>%", size=250, weight = 700, gravity = "center", location = '-380+125', degrees = 345, font = "Fira Code")%>%
+  magick::image_annotate("{ripe}", size=200, gravity = "south", location = '+400+250', degrees = 330, font = "Fira Code")%>%
+  #magick::image_annotate("{ripe}", size=200, gravity = "center", location = '+75-50', degrees = 45, font = "Fira Code")%>%
+  bunny::image_compose(hex_border, gravity = "center", operator = "Over")
 
 img_hex%>%
   magick::image_scale("200x200") %>%
@@ -39,7 +35,7 @@ gh_logo <- bunny::github %>%
 
 gh <- image_canvas_ghcard("#ffffff") %>%
   image_compose(img_hex_gh, gravity = "East", offset = "+50+0") %>%
-  image_annotate("Ripe: Rerun Pipes", gravity = "West", location = "+50-30",
+  image_annotate("Rerun Magrittr Pipes", gravity = "West", location = "+50-30",
                  color="#0d4448", size=60, font="Aller", weight = 700) %>%
   image_compose(gh_logo, gravity="West", offset = "+50+40") %>%
   image_annotate("yonicd/ripe", gravity="West", location="+110+45",
